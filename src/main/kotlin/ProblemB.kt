e8d159fd625322a5358af550fca8a0101db18e37
1Bitcoin <shigalkin@mail.ru>
2021-08-17 15:21:37 +0300

    done ProblemB

--- src/main/kotlin/ProblemB.kt
+++ src/main/kotlin/ProblemB.kt
@@ -2,8 +2,23 @@ class ProblemB {
 
     fun main() {
 
-        //ваша реализация в методе main
+        val t = readInt()
+        var n: Int; var k: Int; var counter: Int
+        var answerString: String
 
+        for (count in 1..t) {
+            val modelString = readListInt()
+            n = modelString[0]; k = modelString[1]; counter = 0
+            answerString = ""
+
+            for (i in 1..n) {
+                if (counter == k)
+                    counter = 0
+
+                answerString += 'a' + counter
+                counter++
+            }
+            println(answerString)
+        }
     }
-
 }
