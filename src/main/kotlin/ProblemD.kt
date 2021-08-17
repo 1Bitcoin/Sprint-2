4f1bc5d2f6eb30809926e37d5e351009c1cb63f3
1Bitcoin <shigalkin@mail.ru>
2021-08-17 13:21:49 +0300

    done ProblemD

--- src/main/kotlin/ProblemD.kt
+++ src/main/kotlin/ProblemD.kt
@@ -2,8 +2,24 @@ class ProblemD {
 
     fun main() {
 
-        //ваша реализация в методе main
+        val t = readInt()
+        var numClients: Int; var n: Int
+        var maxProfit: Long; var currentProfit: Long
 
+        for (count in 1..t) {
+            n = readInt()
+            val numbers = readListLong().sortedDescending()
+            numClients = 1
+            maxProfit = 0
+
+            for (i in numbers.indices) {
+                currentProfit = numClients * numbers[i]
+                if (currentProfit > maxProfit) {
+                    maxProfit = currentProfit
+                }
+                numClients++
+            }
+            println(maxProfit)
+        }
     }
-
 }
