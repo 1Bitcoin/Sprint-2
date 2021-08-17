5904409c781d884603ea9265b0b4ba249302f7f4
1Bitcoin <shigalkin@mail.ru>
2021-08-17 13:46:11 +0300

    done ProblemC

--- src/main/kotlin/ProblemC.kt
+++ src/main/kotlin/ProblemC.kt
@@ -2,8 +2,13 @@ class ProblemC {
 
     fun main() {
 
-        //ваша реализация в методе main
+        val n = readInt()
+        val ratingsList = readListInt().sorted()
+        var countTasks = 0
 
+        for (i in ratingsList.indices step 2) {
+            countTasks += ratingsList[i + 1] - ratingsList[i]
+        }
+        println(countTasks)
     }
-
 }
