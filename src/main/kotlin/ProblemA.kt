00d4a7ff88cb8dfe0352cfde495159c3dc0c3708
1Bitcoin <shigalkin@mail.ru>
2021-08-17 14:48:00 +0300

    done ProblemA

--- src/main/kotlin/ProblemA.kt
+++ src/main/kotlin/ProblemA.kt
@@ -1,10 +1,23 @@
 class ProblemA {
 
     fun main() {
-        print("hello")
 
-        //ваша реализация в методе main
+        val t = readInt()
+        var a: Int; var b: Int; var k: Int
+        var totalPosition: Long
 
+        for (count in 1..t) {
+            val numbersJump = readListInt()
+            a = numbersJump[0]; b = numbersJump[1]; k = numbersJump[2]
+            totalPosition = 0
+
+            for (i in 1..k) {
+                when(i % 2 == 0) {
+                    false -> totalPosition += a
+                    true -> totalPosition -= b
+                }
+            }
+            println(totalPosition)
+        }
     }
-
 }
